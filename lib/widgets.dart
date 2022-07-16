642d08f03a35512bf738e65c396c53ef4ef88af6
winfred <winfred242002@gmail.com>
2022-07-16 16:56:29 +0000

    :running: create widgets.dart

---
+++ lib/widgets.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+Widget currentWeather(IconData icon, String temperature, String city){
+  return Center(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(height: 20),
+        Icon(
+            icon,
+            color:Colors.orange,
+            size: 90
+        ),
+        const SizedBox(height: 30),
+        Text(
+          temperature,
+          style: const TextStyle(
+              fontSize: 32
+          ),
+        ),
+        const SizedBox(height: 10.0,),
+        Text(
+          city,
+          style: const TextStyle(
+              fontSize: 28
+          ),
+        ),
+        const SizedBox(height: 20.0,),
+
+        const SizedBox(height: 20.0,)
+      ],
+    ),
+  );
+}
+
+
+
+
+Widget additionalInformation(String humidity, String pressure, String wind,String feelsLike ){
+  const textFont  = TextStyle(fontWeight: FontWeight.w600, fontSize: 18,);
+  const infoFont  = TextStyle(fontWeight: FontWeight.w400, fontSize: 18,);
+
+  return Container(
+    width: double.infinity,
+    padding: const EdgeInsets.all(18.0),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:const [
+                Text(
+                  "Wind",
+                  style: textFont,
+                ),
+                SizedBox(height: 30.0),
+                Text(
+                  "Humidity",
+                  style: textFont,
+                ),
+                SizedBox(height: 30.0),
+                Text(
+                  "Pressure",
+                  style: textFont,
+                ),
+                SizedBox(height: 30.0),
+                Text(
+                  "Feels Like",
+                  style: textFont,
+                ),
+                SizedBox(height: 30.0),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:[
+                Text(
+                  wind,
+                  style: infoFont,
+                ),
+                const SizedBox(height: 30.0),
+                Text(
+                  humidity,
+                  style: infoFont,
+                ),
+                const SizedBox(height: 30.0),
+                Text(
+                  pressure,
+                  style: infoFont,
+                ),
+                const SizedBox(height: 30.0),
+                Text(
+                  feelsLike,
+                  style: infoFont,
+                ),
+                const SizedBox(height: 30.0),
+              ],
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
