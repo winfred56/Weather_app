5a81eb312c34977ae7150f0c4da9b5302343b9f8
winfred <winfred242002@gmail.com>
2022-07-16 16:55:31 +0000

    :running: create city.dart

---
+++ lib/pages/City.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class City extends StatefulWidget {
+  const City({Key? key}) : super(key: key);
+
+  @override
+  State<City> createState() => _CityState();
+}
+
+class _CityState extends State<City> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
